2c5aa7493573807ed75e003f7da7436a2777a075
Ben <b.bonfil@uu.nl>
2024-04-22 11:35:40 +0200

    fixed whitespace handling in _parse_yum_or_zypper_repositories

--- pyinfra/facts/util/packaging.py
+++ pyinfra/facts/util/packaging.py
@@ -34,7 +34,7 @@ def _parse_yum_or_zypper_repositories(output):
             current_repo["name"] = line[1:-1]
 
         if current_repo and "=" in line:
-            key, value = line.split("=", 1)
+            key, value = re.split(r"\s*=\s*", line, maxsplit=1)
             current_repo[key] = value
 
     if current_repo:
