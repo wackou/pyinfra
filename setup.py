d309ba4bd3368082dac7abaf3f8f8443d46d325d
Nick Mills-Barrett <nick@fizzadar.com>
2024-05-11 18:58:20 +0100

    Start testing against Python 3.12

--- setup.py
+++ setup.py
@@ -145,6 +145,8 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
             "Topic :: System :: Systems Administration",
             "Topic :: System :: Installation/Setup",
             "Topic :: Utilities",
